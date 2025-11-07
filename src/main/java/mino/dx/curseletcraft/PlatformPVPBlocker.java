5d37764dca07b4c198771d72e09d4fdb9572cb5b
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2025-11-07 21:25:24 +0700

    added Folia support (I)

--- src/main/java/mino/dx/curseletcraft/PlatformPVPBlocker.java
+++ src/main/java/mino/dx/curseletcraft/PlatformPVPBlocker.java
@@ -5,13 +5,18 @@ import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import mino.dx.curseletcraft.commands.ReloadCmd;
 import mino.dx.curseletcraft.config.Config;
+import mino.dx.curseletcraft.hook.FoliaSupport;
 import mino.dx.curseletcraft.listeners.DamageListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlatformPVPBlocker extends JavaPlugin {
+
+    private boolean isFoliaServer;
+
     @Override
     public void onEnable() {
         Config.initialize(this);
+        this.isFoliaServer = FoliaSupport.isFolia();
         getServer().getPluginManager().registerEvents(new DamageListener(this), this);
         registerCommands();
         getLogger().info("PlatformPVPBlocker by MinoMC_YTB enabled successfully!");
@@ -25,5 +30,9 @@ public final class PlatformPVPBlocker extends JavaPlugin {
                     LifecycleEvents.COMMANDS,
                     event -> event.registrar().register(reloadCmdNode, commandDescription));
         }
+    }
+
+    public boolean isFoliaServer() {
+        return isFoliaServer;
     }
 }
---
+++ src/main/java/mino/dx/curseletcraft/hook/FoliaSupport.java
@@ -0,0 +1,12 @@
+package mino.dx.curseletcraft.hook;
+
+public class FoliaSupport {
+    public static boolean isFolia() {
+        try {
+            Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+}
